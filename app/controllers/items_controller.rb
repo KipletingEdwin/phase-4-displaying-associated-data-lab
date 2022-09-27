804ca0672730a9668de3f541369de0acb20669a0
Kipleting <kipletingedwin4@gmail.com>
2022-09-27 23:36:41 +0300

    Done

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,7 @@
 class ItemsController < ApplicationController
+    # belongs_to :user
+    def index 
+        items = Item.all 
+        render json: items, include: :user
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,8 @@
-class UsersController < ApplicationController
+class UsersController < ApplicationController 
+    # has_many :items
+    def show 
+        allInfo = User.find(params[:id])
+        render json: allInfo, include: :items
+
+    end
 end
